9c34d92707b300f50465800485442d437900143d
liamscofield <liamscofield@gmail.com>
2015-08-09 13:53:55 +0800

    Initial Commit

---
+++ TeethHelper/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TeethHelper
+//
+//  Created by AlienLi on 15/8/9.
+//  Copyright (c) 2015年 MarcoLi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
