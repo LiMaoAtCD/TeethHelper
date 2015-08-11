dfb28a2b7c39607eb874dcec8968b0a6e7717ca8
liamscofield <liamscofield@gmail.com>
2015-08-11 21:26:33 +0800

    注册 界面

--- TeethHelper/RegisterViewController.m
+++ TeethHelper/RegisterViewController.m
@@ -9,7 +9,22 @@
 #import "RegisterViewController.h"
 #import "Utils.h"
 @interface RegisterViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *nickNameTextField;
 
+@property (weak, nonatomic) IBOutlet UITextField *phoneTextField;
+
+@property (weak, nonatomic) IBOutlet UITextField *verifyCodeTextField;
+
+@property (weak, nonatomic) IBOutlet UITextField *passwordTextField;
+
+@property (weak, nonatomic) IBOutlet UITextField *confirmTextField;
+
+@property (weak, nonatomic) IBOutlet UIButton *verifyCodeButton;
+@property (weak, nonatomic) IBOutlet UIButton *readButton;
+
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+
+@property (nonatomic, strong) UITextField *activeField;
 @end
 
 @implementation RegisterViewController
@@ -18,6 +33,11 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     [Utils ConfigNavigationBarWithTitle:@"注册" onViewController:self];
+
+    [self registerForKeyboardNotifications];
+    UITapGestureRecognizer *tap =[[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(dismissKeyboard)];
+    
+    [self.scrollView addGestureRecognizer:tap];
 }
 
 -(void)pop{
@@ -27,6 +47,68 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+- (IBAction)registerUser:(id)sender {
+}
+
+
+- (void)registerForKeyboardNotifications
+{
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(keyboardWillShown:)
+                                                 name:UIKeyboardWillShowNotification object:nil];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(keyboardWillBeHidden:)
+                                                 name:UIKeyboardWillHideNotification object:nil];
+    
+}
+
+- (void)keyboardWillShown:(NSNotification*)aNotification
+{
+    
+    NSDictionary* info = [aNotification userInfo];
+    CGSize kbSize = [[info objectForKey:UIKeyboardFrameBeginUserInfoKey] CGRectValue].size;
+    
+    UIEdgeInsets contentInsets = UIEdgeInsetsMake(0.0, 0.0, kbSize.height, 0.0);
+    self.scrollView.contentInset = contentInsets;
+    self.scrollView.scrollIndicatorInsets = contentInsets;
+    
+    // If active text field is hidden by keyboard, scroll it so it's visible
+    // Your app might not need or want this behavior.
+    CGRect aRect = self.view.frame;
+    aRect.size.height -= kbSize.height;
+    if (!CGRectContainsPoint(aRect, self.activeField.frame.origin) ) {
+        [self.scrollView scrollRectToVisible:self.activeField.frame animated:YES];
+    }
+}
+
+// Called when the UIKeyboardWillHideNotification is sent
+- (void)keyboardWillBeHidden:(NSNotification*)aNotification
+{
+    UIEdgeInsets contentInsets = UIEdgeInsetsZero;
+    //    UIEdgeInsets contentInsets = UIEdgeInsetsMake(64, 0, 0, 0);
+    
+    self.scrollView.contentInset = contentInsets;
+    self.scrollView.scrollIndicatorInsets = contentInsets;
+}
+- (void)textFieldDidBeginEditing:(UITextField *)textField
+{
+    self.activeField = textField;
+}
+
+- (void)textFieldDidEndEditing:(UITextField *)textField
+{
+    self.activeField = nil;
+}
+
+-(void)dismissKeyboard{
+    [self.nickNameTextField resignFirstResponder];
+    [self.phoneTextField resignFirstResponder];
+    [self.verifyCodeTextField resignFirstResponder];
+    [self.passwordTextField resignFirstResponder];
+    [self.confirmTextField resignFirstResponder];
+    
 }
 
 /*
