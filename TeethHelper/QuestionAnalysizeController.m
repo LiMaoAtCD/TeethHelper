e0048f980c004a702db6e799f5747dd006a9193c
liamscofield <liamscofield@gmail.com>
2015-08-14 14:51:47 +0800

    美白计划

--- TeethHelper/QuestionAnalysizeController.m
+++ TeethHelper/QuestionAnalysizeController.m
@@ -8,7 +8,25 @@
 
 #import "QuestionAnalysizeController.h"
 #import "Utils.h"
+#import <Masonry.h>
+
 @interface QuestionAnalysizeController ()
+
+@property (weak, nonatomic) IBOutlet UILabel *projectLabel;
+
+@property (weak, nonatomic) IBOutlet UIView *threeView;
+@property (weak, nonatomic) IBOutlet UIView *secondView;
+@property (weak, nonatomic) IBOutlet UIView *firstView;
+
+@property (weak, nonatomic) IBOutlet UILabel *timesLabel;
+@property (weak, nonatomic) IBOutlet UILabel *perTimeMinutesLabel;
+@property (weak, nonatomic) IBOutlet UILabel *projectDayLabel;
+
+@property (weak, nonatomic) IBOutlet UIButton *beginTimeButton;
+
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *verticalMargin;
+
+
 
 @end
 
@@ -18,6 +36,46 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
     [Utils ConfigNavigationBarWithTitle:@"美白计划" onViewController:self];
+    
+    _verticalMargin.constant = 30;
+//
+//    CGFloat height = [UIScreen mainScreen].bounds.size.height;
+//    
+//    CGFloat marginHeight = height - 120  - 50;
+//    
+//    
+//    CGFloat viewMargin =( marginHeight - 77 * 3 )/ 4;
+//    
+//    [_firstView mas_makeConstraints:^(MASConstraintMaker *make) {
+//        make.top.equalTo(@(120 + viewMargin));
+//        make.left.equalTo(self.view).offset(0);
+//        make.right.equalTo(self.view).offset(0);
+//        make.height.equalTo(@77);
+//        
+//    }];
+//    [_secondView mas_makeConstraints:^(MASConstraintMaker *make) {
+//        make.top.equalTo(@(120 + 2 * viewMargin + 77));
+//        make.left.equalTo(self.view).offset(0);
+//        make.right.equalTo(self.view).offset(0);
+//        make.height.equalTo(@77);
+//        
+//    }];
+//    [_threeView mas_makeConstraints:^(MASConstraintMaker *make) {
+//        make.top.equalTo(@(120 + 3 * viewMargin));
+//        make.left.equalTo(self.view).offset(0);
+//        make.right.equalTo(self.view).offset(0);
+//        make.height.equalTo(@77);
+//        
+//    }];
+    
+ 
+    
+    
+}
+
+-(void)viewDidAppear:(BOOL)animated{
+    [super viewDidAppear:animated];
+
 }
 
 - (void)didReceiveMemoryWarning {
