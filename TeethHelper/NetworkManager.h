4db04a0e65637d51b575ea40064ab664bc0544d2
liamscofield <liamscofield@gmail.com>
2015-08-18 22:06:13 +0800

    登录注测部分借口

--- TeethHelper/NetworkManager.h
+++ TeethHelper/NetworkManager.h
@@ -7,7 +7,91 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <AFNetworking.h>
+
+typedef void(^NetWorkHandler)(AFHTTPRequestOperation *operation, id responseObject);
+typedef void(^NetWorkFailHandler)(AFHTTPRequestOperation *operation,NSError *error);
 
 @interface NetworkManager : NSObject
+/**
+ *  注册
+ *
+ *  @param nickName   昵称
+ *  @param phone      电话
+ *  @param password   密码
+ *  @param verifyCode 验证码
+ */
++(void)RegisterByNickname:(NSString *)nickName phone:(NSString *)phone password:(NSString *)password verifyCode:(NSString*)verifyCode withCompletionHandler:(NetWorkHandler)completionHandler FailHandler:(NetWorkFailHandler)failHandler;
+
+
+/**
+ *  登录（非三方）
+ *
+ *  @param username          用户名
+ *  @param password          密码
+ *  @param completionHandler
+ *  @param failHandler
+ */
++(void)LoginByUsername:(NSString *)username password:(NSString *)password withCompletionHandler:(NetWorkHandler)completionHandler FailHandler:(NetWorkFailHandler)failHandler;
+
+
+/**
+ *  三方登录
+ *
+ *  @param thirdId           三方唯一ID
+ *  @param provider          提供方：WEIXIN
+ *  @param userAliasName     三方昵称
+ *  @param userAvatar        三方头像
+ *  @param completionHandler
+ *  @param failHandler
+ */
++(void)ThirdLoginByThirdID:(NSString*)thirdId provider:(NSString *)provider userAliasName:(NSString *)userAliasName userAvatar:(NSString *)userAvatar withCompletionHandler:(NetWorkHandler)completionHandler FailHandler:(NetWorkFailHandler)failHandler;
+
+
+/**
+ *  注销
+ *
+ *  @param accessToken       token
+ *  @param completionHandler
+ *  @param failHandler
+ */
++(void)LogoutByToken:(NSString*)accessToken withCompletionHandler:(NetWorkHandler)completionHandler FailHandler:(NetWorkFailHandler)failHandler;
+
+
+/**
+ *  获取注册验证码
+ *
+ *  @param phone             手机号码
+ *  @param completionHandler
+ *  @param failHandler
+ */
++(void)FetchVerifyCode:(NSString *)phone withCompletionHandler:(NetWorkHandler)completionHandler FailHandler:(NetWorkFailHandler)failHandler;
+
+
+/**
+ *  忘记密码获取验证码
+ *
+ *  @param phone             电话号码
+ *  @param completionHandler
+ *  @param failHandler
+ */
++(void)FetchForgetVerifyCode:(NSString *)phone withCompletionHandler:(NetWorkHandler)completionHandler FailHandler:(NetWorkFailHandler)failHandler;
+
+/**
+ *  忘记密码
+ *
+ *  @param phone             手机号
+ *  @param verifyCode        验证码
+ *  @param password          密码
+ *  @param completionHandler
+ *  @param failHandler       
+ */
++(void)ForgetPassword:(NSString*)phone verifyCode:(NSString*)verifyCode password:(NSString*)password withCompletionHandler:(NetWorkHandler)completionHandler FailHandler:(NetWorkFailHandler)failHandler;
+
+
+
+
+
+
 
 @end
--- TeethHelper/NetworkManager.m
+++ TeethHelper/NetworkManager.m
@@ -10,4 +10,57 @@
 
 @implementation NetworkManager
 
+
++(void)RegisterByNickname:(NSString *)nickName phone:(NSString *)phone password:(NSString *)password verifyCode:(NSString*)verifyCode withCompletionHandler:(NetWorkHandler)completionHandler FailHandler:(NetWorkFailHandler)failHandler{
+    NSDictionary *dictionary = @{@"nickName":nickName,@"phone":phone,@"password":password,@"verifyCode":verifyCode};
+    
+    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+    [manager POST:@"http://www.7wang523.com/teeth-api/user/register" parameters:dictionary success:completionHandler failure:failHandler];
+}
+
+
++(void)LoginByUsername:(NSString *)username password:(NSString *)password withCompletionHandler:(NetWorkHandler)completionHandler FailHandler:(NetWorkFailHandler)failHandler{
+    NSDictionary *dictionary = @{@"username":username,@"password":password};
+    
+    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+    [manager POST:@"http://www.7wang523.com/teeth-api/auth/token" parameters:dictionary success:completionHandler failure:failHandler];
+}
+
++(void)ThirdLoginByThirdID:(NSString*)thirdId provider:(NSString *)provider userAliasName:(NSString *)userAliasName userAvatar:(NSString *)userAvatar withCompletionHandler:(NetWorkHandler)completionHandler FailHandler:(NetWorkFailHandler)failHandler{
+    NSDictionary *dictionary = @{@"thirdId":thirdId,@"provider":provider,@"userAliasName":userAliasName,@"userAvatar":userAvatar};
+    
+    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+    [manager POST:@"http://www.7wang523.com/teeth-api/auth/token" parameters:dictionary success:completionHandler failure:failHandler];
+}
+
++(void)LogoutByToken:(NSString*)accessToken withCompletionHandler:(NetWorkHandler)completionHandler FailHandler:(NetWorkFailHandler)failHandler{
+    NSDictionary *dictionary = @{@"accessToken":accessToken};
+    
+    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+    [manager POST:@"http://www.7wang523.com/teeth-api/auth/logout" parameters:dictionary success:completionHandler failure:failHandler];
+}
+
++(void)FetchVerifyCode:(NSString *)phone withCompletionHandler:(NetWorkHandler)completionHandler FailHandler:(NetWorkFailHandler)failHandler{
+    NSDictionary *dictionary = @{@"phone":phone};
+    
+    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+    [manager POST:@"http://www.7wang523.com/teeth-api/user/register/verifycode" parameters:dictionary success:completionHandler failure:failHandler];
+
+}
++(void)FetchForgetVerifyCode:(NSString *)phone withCompletionHandler:(NetWorkHandler)completionHandler FailHandler:(NetWorkFailHandler)failHandler{
+    NSDictionary *dictionary = @{@"phone":phone};
+    
+    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+    [manager POST:@"http://www.7wang523.com/teeth-api/user/verifycode" parameters:dictionary success:completionHandler failure:failHandler];
+}
+
++(void)ForgetPassword:(NSString*)phone verifyCode:(NSString*)verifyCode password:(NSString*)password withCompletionHandler:(NetWorkHandler)completionHandler FailHandler:(NetWorkFailHandler)failHandler{
+    NSDictionary *dictionary = @{@"phone":phone,@"verifyCode":verifyCode,@"password":password};
+    
+    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+    [manager POST:@"http://www.7wang523.com/teeth-api/password/forget" parameters:dictionary success:completionHandler failure:failHandler];
+
+}
+
+
 @end
