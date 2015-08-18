23d4cbafe70ce9116dc3cd7e6d0a7c9370fbf099
liamscofield <liamscofield@gmail.com>
2015-08-18 11:43:49 +0800

    社交主页面布局

--- TeethHelper/SocialViewController.m
+++ TeethHelper/SocialViewController.m
@@ -30,12 +30,31 @@
     self.dataItems = [NSMutableArray array];
 
     self.dataItems =[@[
-                       @{@"avatar":@"xxx",
-                         @"time":@"2015-01-01",
-                         @""
+                       @{
+                           @"avatar":@"xxx",
+                           @"date":[NSDate dateWithTimeInterval:-70 sinceDate:[NSDate date]],
+                           @"containImage":@1
+                         },
+                       @{
+                           @"avatar":@"xxx",
+                           @"date":[NSDate dateWithTimeInterval:-1 sinceDate:[NSDate date]],
+                           @"containImage":@0
+                         },
+                       @{
+                             @"avatar":@"xxx",
+                             @"date":[NSDate dateWithTimeInterval:-5000 sinceDate:[NSDate date]],
+                             @"containImage":@0
+                         },
+                       @{
+                           @"avatar":@"xxx",
+                           @"date":[NSDate dateWithTimeInterval:-3600*27 sinceDate:[NSDate date]],
+                           @"containImage":@0
                          }
                        
                        ] mutableCopy];
+    
+    self.tableView.rowHeight = UITableViewAutomaticDimension;
+    self.tableView.estimatedRowHeight = 44.0;
 }
 
 - (void)didReceiveMemoryWarning {
@@ -49,11 +68,89 @@
 }
 
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
-    return 10;
+    return self.dataItems.count;
 }
 
 -(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    NSDictionary *temp = self.dataItems[indexPath.row];
+    if ([temp[@"containImage"] integerValue] == 1) {
+        SocialImageCell *cell = [tableView dequeueReusableCellWithIdentifier:@"SocialImageCell" forIndexPath:indexPath];
+        
+        NSDate *date = temp[@"date"];
+        
+        if (date.secondsAgo <= 60) {
+            cell.timeStampLabel.text = @"刚刚";
+        } else if (date.minutesAgo <= 60 ){
+            cell.timeStampLabel.text =[NSString stringWithFormat:@"%ld分钟前",(long)date.minutesAgo];
+        } else if (date.hoursAgo <= 24){
+            cell.timeStampLabel.text =[NSString stringWithFormat:@"%ld小时前",(long)date.hoursAgo];
+        } else{
+            
+            NSDateFormatter *MonthFormatter = [[NSDateFormatter alloc] init];
+            [MonthFormatter setDateFormat:@"MM"];
+            NSString *month = [MonthFormatter stringFromDate:date];
+            
+            NSDateFormatter *DayFormatter = [[NSDateFormatter alloc] init];
+            [DayFormatter setDateFormat:@"dd"];
+            NSString *day = [DayFormatter stringFromDate:date];
+            
+            NSDateFormatter *hourFormatter = [[NSDateFormatter alloc] init];
+            [hourFormatter setDateFormat:@"HH"];
+            NSString *hour = [hourFormatter stringFromDate:date];
+            NSDateFormatter *MinuteFormatter = [[NSDateFormatter alloc] init];
+            [MinuteFormatter setDateFormat:@"mm"];
+            NSString *minute = [MinuteFormatter stringFromDate:date];
+            
+            
+            NSString *timestamp = [NSString stringWithFormat:@"%@月%@日 %@:%@",month,day,hour,minute];
+            
+            cell.timeStampLabel.text = timestamp;
+        }
+        
+        cell.cotentLabel.text = @"1刚打开结婚发疯节哈看见奋斗哈健康地方哈家开发和大家康复哈觉得符合啊了就罚款";
+        
+        return cell;
+    } else{
+        SocialNoImageCell *cell = [tableView dequeueReusableCellWithIdentifier:@"SocialNoImageCell" forIndexPath:indexPath];
+        
+        NSDate *date = temp[@"date"];
+        
+        if (date.secondsAgo <= 60) {
+            cell.timeStampLabel.text = @"刚刚";
+        } else if (date.minutesAgo <= 60 ){
+            cell.timeStampLabel.text =[NSString stringWithFormat:@"%ld分钟前",(long)date.minutesAgo];
+        } else if (date.hoursAgo <= 24){
+            cell.timeStampLabel.text =[NSString stringWithFormat:@"%ld小时前",(long)date.hoursAgo];
+        } else{
+            
+            NSDateFormatter *MonthFormatter = [[NSDateFormatter alloc] init];
+            [MonthFormatter setDateFormat:@"MM"];
+            NSString *month = [MonthFormatter stringFromDate:date];
+            
+            NSDateFormatter *DayFormatter = [[NSDateFormatter alloc] init];
+            [DayFormatter setDateFormat:@"dd"];
+            NSString *day = [DayFormatter stringFromDate:date];
+            
+            NSDateFormatter *hourFormatter = [[NSDateFormatter alloc] init];
+            [hourFormatter setDateFormat:@"HH"];
+            NSString *hour = [hourFormatter stringFromDate:date];
+            NSDateFormatter *MinuteFormatter = [[NSDateFormatter alloc] init];
+            [MinuteFormatter setDateFormat:@"mm"];
+            NSString *minute = [MinuteFormatter stringFromDate:date];
+            
+            
+            NSString *timestamp = [NSString stringWithFormat:@"%@月%@日 %@:%@",month,day,hour,minute];
+            
+            cell.timeStampLabel.text = timestamp;
+        }
 
+        cell.contentLabel.text = @"2刚打开结婚发疯节哈看见奋斗哈健康地方哈家开发和大家康复哈觉得符合啊了就罚款";
+        return cell;
+    }
 }
+
+
+
 
 @end
