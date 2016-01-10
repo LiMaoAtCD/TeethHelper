30cba5eece203f9a0baddb9a982b6fda6fe63e41
liamscofield <limitscofield@gmail.com>
2016-01-10 13:06:39 +0800

    是否延长计划 -完成

--- TeethHelper/SuggestionKeepViewController.m
+++ TeethHelper/SuggestionKeepViewController.m
@@ -121,7 +121,7 @@
     NSInteger processDays = [MeiBaiConfigFile getProcessDays];
     NSInteger days = [MeiBaiConfigFile getNeedCureDays];
     
-    if ( processDays != days) {
+    if ( processDays == days) {
         //需要询问是否延长治疗计划
         UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"" message:@"是否延长美白计划?" preferredStyle:UIAlertControllerStyleAlert];
         
