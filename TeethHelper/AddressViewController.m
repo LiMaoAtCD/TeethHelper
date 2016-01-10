949d8ca5fd8363d53e003f63b4daae979613b8c4
liamscofield <limitscofield@gmail.com>
2016-01-10 15:45:09 +0800

    cityPicker import

--- TeethHelper/AddressViewController.m
+++ TeethHelper/AddressViewController.m
@@ -11,11 +11,19 @@
 #import "NetworkManager.h"
 #import <SVProgressHUD.h>
 #import "AccountManager.h"
+#import "TLCityPickerController.h"
 
-@interface AddressViewController ()
+@interface AddressViewController ()<TLCityPickerDelegate>
 
 @property (weak, nonatomic) IBOutlet UITextField *addressTextField;
 //@property (nonatomic, copy) NSString *address;
+
+@property (weak, nonatomic) IBOutlet UIView *addressPrefixView;
+
+@property (weak, nonatomic) IBOutlet UILabel *provinceLabel;
+@property (weak, nonatomic) IBOutlet UILabel *cityLabel;
+
+
 @end
 
 @implementation AddressViewController
@@ -28,7 +36,42 @@
     [self.addressTextField addTarget:self action:@selector(textFieldEditChanged:) forControlEvents:UIControlEventEditingChanged];
     self.addressTextField.text = self.address;
     
+    UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(selectCity)];
+    
+    [self.addressPrefixView addGestureRecognizer:tap];
 }
+
+
+-(void)selectCity{
+//    TLCityPickerController *cityPickerVC = [[TLCityPickerController alloc] init];
+//    [cityPickerVC setDelegate:self];
+//    
+//    cityPickerVC.locationCityID = @"1400010000";
+//    //    cityPickerVC.commonCitys = [[NSMutableArray alloc] initWithArray: @[@"1400010000", @"100010000"]];        // 最近访问城市，如果不设置，将自动管理
+//    cityPickerVC.hotCitys = @[@"100010000", @"200010000", @"300210000", @"600010000", @"300110000"];
+//    
+//    [self presentViewController:[[UINavigationController alloc] initWithRootViewController:cityPickerVC] animated:YES completion:^{
+//        
+//    }];
+
+}
+
+#pragma mark - TLCityPickerDelegate
+- (void) cityPickerController:(TLCityPickerController *)cityPickerViewController didSelectCity:(TLCity *)city
+{
+//    [self.cityPickerButton setTitle:city.cityName forState:UIControlStateNormal];
+    [cityPickerViewController dismissViewControllerAnimated:YES completion:^{
+        
+    }];
+}
+
+- (void) cityPickerControllerDidCancel:(TLCityPickerController *)cityPickerViewController
+{
+    [cityPickerViewController dismissViewControllerAnimated:YES completion:^{
+        
+    }];
+}
+
 -(void)textFieldEditChanged:(UITextField *)textField{
     
     if (textField.text.length > 50) {
