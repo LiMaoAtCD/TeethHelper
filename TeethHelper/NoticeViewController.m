e00d7e774f149570c0064dd5acbe31527a6e0756
liamscofield <limitscofield@gmail.com>
2016-01-28 15:21:24 +0800

    注意事项

--- TeethHelper/NoticeViewController.m
+++ TeethHelper/NoticeViewController.m
@@ -60,7 +60,7 @@
     cell.indexLabel.text = [NSString stringWithFormat:@"%ld",indexPath.row + 1];
     cell.contentLabel.text = self.items[indexPath.row];
     
-    
+    [cell updateConstraintsIfNeeded];
     
     return cell;
 }
